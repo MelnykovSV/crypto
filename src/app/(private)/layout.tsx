84ecae7db1c790ed0d099dcbfa80c1f82ee87eee
MelnykovSV <svmelnykov@gmail.com>
2024-01-26 14:07:13 +0200

    1

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -3,9 +3,10 @@ import { MobileMenuContainer, AsideMenu, UserPreview } from "@/components";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 // import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../../../options";
 
 export default async function Layout({ children }: any) {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/");
   }
